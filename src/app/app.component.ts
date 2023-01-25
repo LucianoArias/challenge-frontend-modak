8664b8b1da8dba53ab22ff9d1d208c4c668d712f
LucianoArias <lucianoarias1997@gmail.com>
2023-01-25 17:21:00 -0300

    final proyect

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,49 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+
+interface Gender {
+  value: string;
+  viewValue: string;
+}
+
+interface Payment {
+  value: string;
+  viewValue: string;
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'challenge-modak';
+  firstFormGroup: FormGroup;
+  secondFormGroup: FormGroup;
+  thirdFormGroup: FormGroup;
+
+  constructor(private _formBuilder: FormBuilder) {
+    this.firstFormGroup = this._formBuilder.group({
+      dateOfBirth: ['', Validators.required],
+      gender: ['', Validators.required],
+      idNumber: ['', Validators.required],
+    });
+    this.secondFormGroup = this._formBuilder.group({
+      address: ['', Validators.required],
+    });
+    this.thirdFormGroup = this._formBuilder.group({
+      payment: ['', Validators.required],
+    });
+  }
+
+  genders: Gender[] = [
+    { value: 'male-0', viewValue: 'Male' },
+    { value: 'female-1', viewValue: 'Female' },
+    { value: 'prefernotsay-2', viewValue: 'I prefer not to say' },
+  ];
+
+  payments: Payment[] = [
+    { value: 'cash-0', viewValue: 'Cash' },
+    { value: 'credit-1', viewValue: 'Credit Card' },
+    { value: 'paypal-2', viewValue: 'Paypal' },
+  ];
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,34 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MatStepperModule } from '@angular/material/stepper';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatSelectModule } from '@angular/material/select';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatStepperModule,
+    MatFormFieldModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatInputModule,
+    MatButtonModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatSelectModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
